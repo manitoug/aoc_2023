4bf7527431fb9395c3b8732d549e35f24ff027d5
Mathieu Gardette <mathieu.gardette@mines-ales.org>
2024-04-18 21:18:15 +0200

    ADD: Day 1

---
+++ advent_of_code_2023/README.md
@@ -0,0 +1,4 @@
+# Advent of Code 2023
+
+This repository is my code written for the advent of code 2023. It is packaged
+using poetry so feel free to download and test it.
---
+++ advent_of_code_2023/day1/main.py
@@ -0,0 +1,36 @@
+#! python
+import re
+import sys
+import argparse
+
+def parse_file(file_location: str):
+
+    with open(file_location, 'r+', encoding='utf-8' ) as f:
+        lines = [line.strip("\n") for line in f.readlines()]
+
+    return lines
+
+def compute_calibration(obfuscated_list) -> int:
+
+    pattern = r"\d"
+    number_lines = ["".join(re.findall(pattern, line)) for line in obfuscated_list]
+    not_empty = filter(lambda line: len(line)>0, number_lines)
+    coordinates = list(map(lambda line: int(f"{line[0]}{line[-1]}"),not_empty))
+
+    return sum(coordinates)
+
+
+def main(file_location: str):
+    raw_lines = parse_file(file_location)
+    coordinates = compute_calibration(raw_lines)
+
+    sys.stdout.write(f'Unobfuscated Coordinates are: {coordinates}')
+
+if __name__=="__main__":
+
+    parser = argparse.ArgumentParser(description='AoC Day 1.')
+    parser.add_argument("file_location", type=str, help="The location of the file to process")
+
+    args = parser.parse_args()
+
+    main(args.file_location)
