3e1a5fac797c9e1b9f2fe754f171d2c2033d907b
Mathieu Gardette <mathieu.gardette@mines-ales.org>
2024-04-18 21:25:20 +0200

    FIX: Linting

--- day1/main.py
+++ day1/main.py
@@ -1,21 +1,23 @@
 #! python
+import argparse
 import re
 import sys
-import argparse
+
 
 def parse_file(file_location: str):
 
-    with open(file_location, 'r+', encoding='utf-8' ) as f:
+    with open(file_location, "r+", encoding="utf-8") as f:
         lines = [line.strip("\n") for line in f.readlines()]
 
     return lines
+
 
 def compute_calibration(obfuscated_list) -> int:
 
     pattern = r"\d"
     number_lines = ["".join(re.findall(pattern, line)) for line in obfuscated_list]
-    not_empty = filter(lambda line: len(line)>0, number_lines)
-    coordinates = list(map(lambda line: int(f"{line[0]}{line[-1]}"),not_empty))
+    not_empty = filter(lambda line: len(line) > 0, number_lines)
+    coordinates = list(map(lambda line: int(f"{line[0]}{line[-1]}"), not_empty))
 
     return sum(coordinates)
 
@@ -24,12 +26,15 @@ def main(file_location: str):
     raw_lines = parse_file(file_location)
     coordinates = compute_calibration(raw_lines)
 
-    sys.stdout.write(f'Unobfuscated Coordinates are: {coordinates}')
+    sys.stdout.write(f"Unobfuscated Coordinates are: {coordinates}")
 
-if __name__=="__main__":
 
-    parser = argparse.ArgumentParser(description='AoC Day 1.')
-    parser.add_argument("file_location", type=str, help="The location of the file to process")
+if __name__ == "__main__":
+
+    parser = argparse.ArgumentParser(description="AoC Day 1.")
+    parser.add_argument(
+        "file_location", type=str, help="The location of the file to process"
+    )
 
     args = parser.parse_args()
 
